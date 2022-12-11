640322b913652eb1dce197826e409f9ccff8f3ff
Darksiend <logosmk2@gmail.com>
2022-12-11 15:22:48 +0200

    Work

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,12 +4,16 @@ import { Button } from "primereact/button";
 import "primereact/resources/themes/lara-light-indigo/theme.css"; //theme
 import "primereact/resources/primereact.min.css"; //core css
 import "primeicons/primeicons.css";
+import { Link } from "react-router-dom";
 export const Header = () => {
   return (
     <div className="header">
       <div className="logo">My Weather App</div>
       <div className="buttons">
-        <Button label="Home"></Button>
+        <Link to="/">
+          <Button label="Home"> </Button>
+        </Link>
+
         <Button label="Favorites"></Button>
       </div>
     </div>
