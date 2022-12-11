a3031fce1296b09021ab6cc8003e8b1610c99cc0
Darksiend <logosmk2@gmail.com>
2022-12-11 15:37:18 +0200

    Work

---
+++ src/components/SearchBar/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+const SearchBar = () => {
+  return (
+    <>
+      <input className="SearchBar" type="text" placeholder="Type Here..." />
+    </>
+  );
+};
+
+export default SearchBar;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,12 @@
 import React from "react";
-
+import SearchBar from "../../components/SearchBar";
+import "./style.css";
 export const Home = () => {
-  return <div>Home!</div>;
+  return (
+    <div className="Home">
+      <SearchBar />
+    </div>
+  );
 };
 
 export default Home;
